b74fe6c24dc8513eab7fd000019db19598062192
Haticekar <hatticekar14@gmail.com>
2024-11-06 09:54:09 +0300

    Initial Commit

---
+++ LandmarksIki/LandmarksIkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksIkiApp.swift
+//  LandmarksIki
+//
+//  Created by Hatice Kar on 6.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksIkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LandmarksIkiTests/LandmarksIkiTests.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksIkiTests.swift
+//  LandmarksIkiTests
+//
+//  Created by Hatice Kar on 6.11.2024.
+//
+
+import Testing
+@testable import LandmarksIki
+
+struct LandmarksIkiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
